28c2a788b5e521ae17a9c4937d3b75e06ff65acb
Tyler <tha7556@users.noreply.github.com>
2016-04-11 03:13:45 -0400

    Update Level.java
    Added Background Music

--- Game/Level.java
+++ Game/Level.java
@@ -2,21 +2,32 @@ package Game;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class Level 
 {
 	private ArrayList<Area> areas;
 	private Area currentArea;
 	private Gameboard gameboard;
+	private String musicFile;
 	
-	public Level(ArrayList<Area> areas, Gameboard gameboard)
+	public Level(ArrayList<Area> areas, Gameboard gameboard, String musicFile)
 	{
 		this.areas = areas;
 		this.gameboard = gameboard;
+		this.musicFile = musicFile;
 		if(areas.size() > 0)
 		{
 			this.currentArea = areas.get(0);
 			displayArea(currentArea,gameboard.getGraphics(), gameboard);
 		}
+	}
+	public Level(ArrayList<Area> areas, Gameboard gameboard)
+	{
+		this(areas, gameboard, null);
 	}
 	public void addArea(Area area)
 	{
@@ -39,7 +50,29 @@ public class Level
 		if(area != null)
 		{
 			area.draw(g, comp);
+			if(musicFile != null)
+				playMusic();
 		}
+	}
+	public void playMusic()
+	{
+		 try {
+		        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(musicFile).getAbsoluteFile());
+		        Clip clip = AudioSystem.getClip();
+		        clip.open(audioInputStream);
+		        clip.loop(15);
+		        clip.start();
+		    } catch(Exception ex) {
+		        ex.printStackTrace();
+		    }
+	}
+	public String getMusic()
+	{
+		return musicFile;
+	}
+	public void setMusic(String music)
+	{
+		this.musicFile = music;
 	}
 	public Gameboard getGameboard()
 	{
