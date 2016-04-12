8dd1e0b5693381b8adb84dc1e57ddb16a7917b8b
Tyler <tha7556@uncw.edu>
2016-04-12 14:00:05 -0400

    Fixed Koopa Size

--- Character/Koopa.java
+++ Character/Koopa.java
@@ -15,7 +15,7 @@ public class Koopa extends Enemy
 	 * @param game The instance of the Game
 	 */
 	public Koopa(int x, int y, Game game) {
-		super(x, y, 16, 27, true,"KoopaWalkLeft.gif", game);
+		super(x, y, 32, 54, true,"KoopaWalkLeft.gif", game);
 		
 	}
 	
