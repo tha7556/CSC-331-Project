c32129de4b4d724011cee4875d9e26f6d7baed95
Tyler <tha7556@uncw.edu>
2016-04-17 02:43:44 -0400

    Scoreboard now works

--- Game/Scoreboard.java
+++ Game/Scoreboard.java
@@ -1,21 +1,47 @@
 package Game;
 
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 /**
  * The Scoreboard that displays Mario's lives, the Time, and the player's points
+ * <br>Extends JComponent</br>
  *
  */
-public class Scoreboard 
+public class Scoreboard extends JComponent
 {
+	private static final long serialVersionUID = 1L;
 	private int lives;
 	private int score;
+	private int coins;
+	private ImageIcon image;
+	private Game game;
 	/**
 	 * Creates a new Scoreboard with score at 0 and lives at 5
 	 */
-	public Scoreboard()
+	public Scoreboard(Game game)
 	{
+		super();
+		setPreferredSize(new Dimension(960,50));
 		score = 0;
+		coins = 0;
 		lives = 5;
+		image = new ImageIcon("ScoreBoard.png");
+		this.game = game;
+	}
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		String displayScore = String.format("%08d", score);
+		image.paintIcon(this, g, 0, 0);
+		g.setFont(new Font("Dialogue",Font.BOLD,20));
+		g.drawString(lives + "", 95, 47);
+		g.drawString(coins + "", 460, 25);
+		g.drawString(displayScore + "", 410, 45);
+		g.drawString(game.getDisplayTime()+"", 830, 30);
 	}
 	/**
 	 * 
@@ -42,19 +68,11 @@ public class Scoreboard
 		this.score = score;
 	}
 	/**
-	 * 
-	 * @param points The amount of points to be added
+	 * Adds 1 coin to Mario and rewards him with 100 points
 	 */
-	public void addPoints(int points)
+	public void addCoin()
 	{
-		score += points;
-	}
-	/**
-	 * Displays the Scoreboard
-	 * @param g The Graphics object
-	 */
-	public void draw(Graphics g)
-	{
-		//Draw itself
+		coins++;
+		score += 100;
 	}
 }
