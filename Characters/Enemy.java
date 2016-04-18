8da57f1ac333ac08f5f134af94b8e31d2832994e
Tyler <tha7556@uncw.edu>
2016-04-17 22:04:58 -0400

    Moved tick and die into Enemy

--- Characters/Enemy.java
+++ Characters/Enemy.java
@@ -81,10 +81,93 @@ public class Enemy extends Character{
 	public Rectangle getBoundsRight(){
 		return new Rectangle(getX()-5+width,getY()+10, 5, height-20);
 	}
+	/**
+	 * Generic Enemy Movement
+	 */
 	@Override
-	public void tick() {
-		// TODO Auto-generated method stub
+	public void tick() 
+	{
+		if(dead) //Death Animation
+		{
+			if(game.getLoopNumber() - dieStart > 10)
+			{
+				alive = false;
+				die();
+			}
+			return;
+		}
+		x += velX;
+		y += velY;
+		for(Obstacle t: game.getCurrentLevel().getCurrentArea().getObstacles()){
+			if(!t.isSolid()) break;
+				if(getBoundsBottom().intersects(t.getBounds())){
+					gravity = 0.0;
+					falling = false;
+					setVelY(0);
+				}
+				else{
+					falling = true;
+				}
+				if(getBoundsLeft().intersects(t.getBounds())){
+					setVelX(-velX);
+					setImage("KoopaWalkRight.gif");
+
+				}
+				if(getBoundsRight().intersects(t.getBounds())){
+					setVelX(-velX);
+					setImage("KoopaWalkLeft.gif");
+
+				}
+			
+		}
+		if(falling){
+			gravity += 0.2;
+			setVelY((int) gravity);
+		}
+		for(Enemy coEnemies: game.getCurrentLevel().getCurrentArea().getEnemies()){
+			if(coEnemies.isSolid())
+			{
+			
+				if(getBoundsRight().intersects(coEnemies.getBoundsLeft())){
+					setVelX(-(getVelX()));
+					setImage("KoopaWalkLeft.gif");
+
+				}
+				if(getBoundsLeft().intersects(coEnemies.getBoundsRight())){
+					setVelX(-(getVelX()));
+					setImage("KoopaWalkRight.gif");
+				}
+				if(getBoundsBottom().intersects(coEnemies.getBoundsTop())){
+					jumping = true;
+					falling = false;
+				}
+			}
+				
+			
+		}
 		
+		if(x <= 0){
+			x = 0;
+			setVelX(1);
+		}
+		if(x + width >= game.getWidth()){
+			setVelX(-1);
+			x = game.getWidth() - (width+1);
+		}
+		
+	}
+	@Override
+	public void die(){
+		if(!alive)
+		{
+			game.getCurrentLevel().getCurrentArea().removeCharacter(this);			
+		}
+		else
+		{
+			dead = true;
+			dieStart = game.getLoopNumber();
+			this.setImage("Poof.gif");
+		}
 	}
 	
 }
