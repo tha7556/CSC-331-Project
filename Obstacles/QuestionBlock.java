b06b99a35c338abf772e2b89b5f0fee2294bda97
Tyler <tha7556@uncw.edu>
2016-04-17 22:08:45 -0400

    Added Javadoc and dispensed variable

--- Obstacles/QuestionBlock.java
+++ Obstacles/QuestionBlock.java
@@ -12,6 +12,7 @@ public class QuestionBlock extends Obstacle
 	private boolean coin;
 	private boolean mushroom;
 	private boolean life;
+	private boolean dispensed = false;
 	/**
 	 * 
 	 * @param x The x location on the Gameboard
@@ -37,14 +38,43 @@ public class QuestionBlock extends Obstacle
 	public void die()
 	{
 		this.setImage("Brown Block.png");
+		this.setDispensed(true);
 	}
+	/**
+	 * 
+	 * @return True if the block contains a coin
+	 */
 	public boolean hasCoin() {
 		return coin;
 	}
+	/**
+	 * 
+	 * @return True if the block contains a mushroom
+	 */
 	public boolean hasMushroom(){
 		return mushroom;
 	}
+	/**
+	 * 
+	 * @return True if the block contains an extra life
+	 */
 	public boolean hasLife(){
 		return life;
+	}
+	/**
+	 * 
+	 * @return True if the block has already been hit
+	 */
+	public boolean isDispensed()
+	{
+		return dispensed;
+	}
+	/**
+	 * 
+	 * @param d True if the block has already been hit, false otherwise
+	 */
+	public void setDispensed(boolean d)
+	{
+		dispensed = d;
 	}
 }
