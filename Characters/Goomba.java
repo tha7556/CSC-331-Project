2b9a3961d7be0c02854bf20b40bd3594590e19e7
Tyler <tha7556@uncw.edu>
2016-04-17 22:05:10 -0400

    Created Goomba class

---
+++ Characters/Goomba.java
@@ -0,0 +1,87 @@
+package Characters;
+
+import Game.Game;
+import Obstacles.Obstacle;
+/**
+ * A basic Enemy
+ *
+ */
+public class Goomba extends Enemy
+{
+	public Goomba(int x, int y, Game game) 
+	{
+		super(x, y, 32, 32, true,"GoombaWalkLeft.gif", game);
+	}
+	@Override
+	public void tick() 
+	{
+		if(dead) //Death Animation
+		{
+			if(game.getLoopNumber() - dieStart > 10)
+			{
+				alive = false;
+				die();
+			}
+			return;
+		}
+		x += velX;
+		y += velY;
+		for(Obstacle t: game.getCurrentLevel().getCurrentArea().getObstacles()){
+			if(!t.isSolid()) break;
+				if(getBoundsBottom().intersects(t.getBounds())){
+					gravity = 0.0;
+					falling = false;
+					setVelY(0);
+				}
+				else{
+					falling = true;
+				}
+				if(getBoundsLeft().intersects(t.getBounds())){
+					setVelX(-velX);
+					setImage("GoombaWalkRight.gif");
+
+				}
+				if(getBoundsRight().intersects(t.getBounds())){
+					setVelX(-velX);
+					setImage("GoombaWalkLeft.gif");
+
+				}
+			
+		}
+		if(falling){
+			gravity += 0.2;
+			setVelY((int) gravity);
+		}
+		for(Enemy coEnemies: game.getCurrentLevel().getCurrentArea().getEnemies()){
+			if(coEnemies.isSolid())
+			{
+			
+				if(getBoundsRight().intersects(coEnemies.getBoundsLeft())){
+					setVelX(-(getVelX()));
+					setImage("GoombaWalkLeft.gif");
+
+				}
+				if(getBoundsLeft().intersects(coEnemies.getBoundsRight())){
+					setVelX(-(getVelX()));
+					setImage("GoombaWalkRight.gif");
+				}
+				if(getBoundsBottom().intersects(coEnemies.getBoundsTop())){
+					jumping = true;
+					falling = false;
+				}
+			}
+				
+			
+		}
+		
+		if(x <= 0){
+			x = 0;
+			setVelX(1);
+		}
+		if(x + width >= game.getWidth()){
+			setVelX(-1);
+			x = game.getWidth() - (width+1);
+		}
+		
+	}
+}
