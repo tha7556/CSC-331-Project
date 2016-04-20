0c6918e5c29ab04aca8172ea8a628a6517949a98
Tyler <tha7556@uncw.edu>
2016-04-20 16:31:33 -0400

    Items now move like Enemies

--- Items/Item.java
+++ Items/Item.java
@@ -6,8 +6,10 @@ import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 
+import Characters.Enemy;
 import Game.Game;
 import Game.Gameboard;
+import Obstacles.Obstacle;
 /**
  * Abstract Item class used to make coins extra lives, 
  * 
@@ -19,6 +21,8 @@ public abstract class Item
 	protected ImageIcon image;
 	protected boolean visible, alive;
 	protected Game game;
+	protected double gravity = 0.0;
+	protected boolean falling;
 	/**
 	 * 
 	 * @param x X location on the Gameboard
@@ -181,7 +185,90 @@ public abstract class Item
 		}
 		
 	}
+	/**
+	 * Removes the Item from the Area
+	 */
 	public void die() {
 		game.getCurrentLevel().getCurrentArea().removeItem(this);		
+	}
+	/**
+	 * 
+	 * @return The Rectangle representing the Bottom bound
+	 */
+	public Rectangle getBoundsBottom(){
+		return new Rectangle(getX()+5,getY()+height,width-5,5);
+	}
+	/**
+	 * 
+	 * @return The Rectangle representing the Left bound
+	 */
+	public Rectangle getBoundsLeft(){
+		return new Rectangle(getX()+2,getY()+10, 5, height-20);
+	}
+	/**
+	 * 
+	 * @return The Rectangle representing the Right bound
+	 */
+	public Rectangle getBoundsRight(){
+		return new Rectangle(getX()-5+width,getY()+10, 5, height-20);
+	}
+	/**
+	 * Is called every time the game loops, allows movement
+	 */
+	public void tick()
+	{
+		x += velX;
+		y += velY;
+		for(Obstacle t: game.getCurrentLevel().getCurrentArea().getObstacles()){
+			if(!t.isSolid()) break;
+				if(getBoundsBottom().intersects(t.getBounds())){
+					gravity = 0.0;
+					falling = false;
+					setVelY(0);
+				}
+				else{
+					falling = true;
+				}
+				if(getBoundsLeft().intersects(t.getBounds())){
+					setVelX(-velX);
+					x = t.getX()+t.getWidth();
+	
+				}
+				if(getBoundsRight().intersects(t.getBounds())){
+					setVelX(-velX);
+					x = t.getX()-40;
+	
+				}
+			
+		}
+		if(falling){
+			gravity += 0.2;
+			setVelY((int) gravity);
+		}
+		for(Enemy coEnemies: game.getCurrentLevel().getCurrentArea().getEnemies()){
+			if(coEnemies.isSolid())
+			{
+			
+				if(getBoundsRight().intersects(coEnemies.getBoundsLeft())){
+					setVelX(-(getVelX()));
+	
+				}
+				if(getBoundsLeft().intersects(coEnemies.getBoundsRight())){
+					setVelX(-(getVelX()));
+				}
+			}
+				
+			
+		}
+		
+		if(x <= 0){
+			x = 0;
+			setVelX(-velX);
+		}
+		if(x + width >= game.getWidth()){
+			setVelX(-velX);
+			x = game.getWidth() - (width+1);
+		}
+		
 	}
 }
